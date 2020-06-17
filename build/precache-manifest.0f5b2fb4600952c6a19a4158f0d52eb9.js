f9f086ed3db1dc8e4187c476785060857a7921b2
ABDULRAQEEB333 <abdulraqeeb133@gmail.com>
2020-06-17 13:09:32 +0500

    update

---
+++ build/precache-manifest.0f5b2fb4600952c6a19a4158f0d52eb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac06a3866275e1057e5863d324bae9c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "627822cecbda0de743b0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b2dd50820a80706c1ca",
+    "url": "/static/js/2.8a4870ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a4870ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627822cecbda0de743b0",
+    "url": "/static/js/main.8880d13f.chunk.js"
+  },
+  {
+    "revision": "b0409bdac96b6fc2d9ae",
+    "url": "/static/js/runtime-main.3c713e71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
